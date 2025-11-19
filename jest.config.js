1e3630619734bfc409b29a6bcd70a5cbfb7863c0
Saraluk Kaiwansakul <saralukkai@gmail.com>
2025-11-19 15:13:06 +0700

    Configure Jest with TypeScript support, add Husky for Git hooks

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 const config = {
+  preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
   },
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
